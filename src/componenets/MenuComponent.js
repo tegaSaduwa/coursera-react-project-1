3cc7a2176853f0e071fb4454680c31c5bd6bd804
Oghenetega Peter-Saduwa <Oghenetega.peter@wemabank.com>
2020-10-06 09:20:20 +0100

    Navbar and Menu Component

---
+++ src/componenets/MenuComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class MenuComponent extends Component {
+
+    constructor (props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default MenuComponent
