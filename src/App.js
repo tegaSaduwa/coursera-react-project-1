d86f8d332a05b19f60f6174d01ef3fedc2455e3d
Oghenetega Peter-Saduwa <Oghenetega.peter@wemabank.com>
2020-10-06 09:35:03 +0100

    components part1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './componenets/MenuComponent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+           
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
